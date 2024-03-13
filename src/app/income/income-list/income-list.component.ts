4188bec07d0d8dfc645330a26838e66817aea6c2
Ambinintsoa <rahmbints@gmail.com>
2024-03-13 16:17:51 +0300

    Fix bug on amountTotal update

--- src/app/income/income-list/income-list.component.ts
+++ src/app/income/income-list/income-list.component.ts
@@ -9,15 +9,25 @@ import { IncomeService } from '../income.service';
 })
 export class IncomeListComponent implements OnInit {
   income: Income[] = [];
+  totalIncome: number = 0;
 
   constructor(private incomeService: IncomeService) {}
 
   ngOnInit(): void {
     this.getIncomeList();
+    this.getTotalIncome();
   }
 
   private getIncomeList() {
-    this.income = this.incomeService.getIncome();
+    this.incomeService.getIncome().subscribe((incomeList) => {
+      this.income = incomeList;
+    });
+  }
+
+  private getTotalIncome() {
+    this.incomeService.getTotalIncome().subscribe((totalIncome) => {
+      this.totalIncome = totalIncome;
+    });
   }
 
   removeIncomeFromList(incomeId: number) {
--- src/app/income/income.service.ts
+++ src/app/income/income.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, map, Observable } from 'rxjs';
 import { Income } from './income';
 import { MOCK_INCOMES } from './mock-income-list';
 
@@ -7,15 +8,19 @@ import { MOCK_INCOMES } from './mock-income-list';
 })
 export class IncomeService {
   private incomeList: Income[] = MOCK_INCOMES;
+  private incomeListSubject: BehaviorSubject<Income[]> = new BehaviorSubject<
+    Income[]
+  >(this.incomeList);
 
   constructor() {}
 
-  getIncome(): Income[] {
-    return this.incomeList;
+  getIncome(): Observable<Income[]> {
+    return this.incomeListSubject.asObservable();
   }
 
   addIncome(newIncome: Income) {
     this.incomeList.push(newIncome);
+    this.incomeListSubject.next(this.incomeList);
   }
 
   deleteIncome(incomeId: number) {
@@ -23,6 +28,15 @@ export class IncomeService {
 
     if (index !== -1) {
       this.incomeList.splice(index, 1);
+      this.incomeListSubject.next(this.incomeList);
     }
+  }
+
+  getTotalIncome(): Observable<number> {
+    return this.incomeListSubject.pipe(
+      map((incomeList) =>
+        incomeList.reduce((total, income) => total + income.amount, 0)
+      )
+    );
   }
 }
